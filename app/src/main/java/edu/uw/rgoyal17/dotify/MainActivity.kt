8065e49edab981c3b99ab4d836b143305f6a9931
rgoyal17 <rgoyal17@uw.edu>
2021-04-12 04:43:09 -0700

    all functionalities completed

--- app/src/main/java/edu/uw/rgoyal17/dotify/MainActivity.kt
+++ app/src/main/java/edu/uw/rgoyal17/dotify/MainActivity.kt
@@ -3,21 +3,22 @@ package edu.uw.rgoyal17.dotify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private var randomNumber = Random.nextInt(0, 1000)
-    private lateinit var songCounter: TextView
+    private lateinit var btn: Button
+    private lateinit var username: TextView
+    private lateinit var editUsername: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        songCounter = findViewById(R.id.textViewCounter)
+        val songCounter = findViewById<TextView>(R.id.textViewCounter)
         songCounter.text = "Song Counter: $randomNumber"
 
         val playImage = findViewById<ImageView>(R.id.imageViewPlay)
@@ -34,6 +35,27 @@ class MainActivity : AppCompatActivity() {
         val prevImage = findViewById<ImageView>(R.id.imageViewPrevious)
         prevImage.setOnClickListener {
             Toast.makeText(this, "Skipping to previous track!", Toast.LENGTH_SHORT).show()
+        }
+
+        username = findViewById(R.id.textViewUsername)
+        editUsername = findViewById(R.id.editTextUsername)
+
+        btn = findViewById(R.id.buttonChangeUser)
+        btn.setOnClickListener() {
+            btnClicked()
+        }
+    }
+
+    fun btnClicked() {
+        if (btn.text == "CHANGE USER") {
+            username.visibility = View.GONE
+            editUsername.visibility = View.VISIBLE
+            btn.text = "APPLY"
+        } else {
+            editUsername.visibility = View.GONE
+            username.visibility = View.VISIBLE
+            username.text = editUsername.text
+            btn.text = "CHANGE USER"
         }
     }
 }
