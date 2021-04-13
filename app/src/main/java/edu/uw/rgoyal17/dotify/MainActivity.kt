8b4c89499a8d25a44e450eb313f0f8ed44f21c9f
rgoyal17 <rgoyal17@uw.edu>
2021-04-12 18:20:14 -0700

    hw1 completed

--- app/src/main/java/edu/uw/rgoyal17/dotify/MainActivity.kt
+++ app/src/main/java/edu/uw/rgoyal17/dotify/MainActivity.kt
@@ -1,61 +1,70 @@
 package edu.uw.rgoyal17.dotify
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import edu.uw.rgoyal17.dotify.databinding.ActivityMainBinding
 import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
-    private var randomNumber = Random.nextInt(0, 1000)
-    private lateinit var btn: Button
-    private lateinit var username: TextView
-    private lateinit var editUsername: EditText
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
 
-        val songCounter = findViewById<TextView>(R.id.textViewCounter)
-        songCounter.text = "Song Counter: $randomNumber"
+        // set number of plays to a random number
+        var randomNumber = Random.nextInt(0, 1000)
+        binding.textViewCounter.text = "$randomNumber plays"
 
-        val playImage = findViewById<ImageView>(R.id.imageViewPlay)
-        playImage.setOnClickListener {
+        // increment the count of the number of plays
+        binding.imageViewPlay.setOnClickListener {
             randomNumber += 1
-            songCounter.text = "Song Counter: $randomNumber"
+            binding.textViewCounter.text = "$randomNumber plays"
         }
 
-        val nextImage = findViewById<ImageView>(R.id.imageViewNext)
-        nextImage.setOnClickListener {
-            Toast.makeText(this, "Skipping to next track!", Toast.LENGTH_SHORT).show()
+        // Toast a message when clicked on next button
+        binding.imageViewNext.setOnClickListener {
+            Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
         }
 
-        val prevImage = findViewById<ImageView>(R.id.imageViewPrevious)
-        prevImage.setOnClickListener {
-            Toast.makeText(this, "Skipping to previous track!", Toast.LENGTH_SHORT).show()
+        // Toast a message when clicked on previous button
+        binding.imageViewPrevious.setOnClickListener {
+            Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
         }
 
-        username = findViewById(R.id.textViewUsername)
-        editUsername = findViewById(R.id.editTextUsername)
-
-        btn = findViewById(R.id.buttonChangeUser)
-        btn.setOnClickListener() {
+        // call btnClicked() when button is clicked
+        binding.buttonChangeUser.setOnClickListener {
             btnClicked()
+        }
+
+        // change color of play count upon long press on cover image
+        binding.imageViewDotify.setOnLongClickListener {
+            binding.textViewCounter.setTextColor(Color.RED)
+            true
         }
     }
 
+
     fun btnClicked() {
-        if (btn.text == "CHANGE USER") {
-            username.visibility = View.GONE
-            editUsername.visibility = View.VISIBLE
-            btn.text = "APPLY"
+        if (binding.buttonChangeUser.text == "CHANGE USER") {
+            binding.textViewUsername.visibility = View.GONE
+            binding.editTextUsername.visibility = View.VISIBLE
+            binding.buttonChangeUser.text = "APPLY"
         } else {
-            editUsername.visibility = View.GONE
-            username.visibility = View.VISIBLE
-            username.text = editUsername.text
-            btn.text = "CHANGE USER"
+            if (binding.editTextUsername.text.toString() == "") {
+                Toast.makeText(this, "Please enter a username!", Toast.LENGTH_SHORT).show()
+            } else {
+                binding.editTextUsername.visibility = View.GONE
+                binding.textViewUsername.visibility = View.VISIBLE
+                binding.textViewUsername.text = binding.editTextUsername.text
+                binding.buttonChangeUser.text = "CHANGE USER"
+            }
         }
     }
 }
